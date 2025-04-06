64b405f357590b5327ac2330032316df74e99058
Dev-socar <omar.fsc19@gmail.com>
2025-04-06 02:58:42 -0600

    fix

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -47,6 +47,6 @@ class RegisterController extends Controller
         auth()->attempt($request->only('email', 'password'));
 
         //redireccionar
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
